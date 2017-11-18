b73652fc74305f17353bc61cdf717195b7dd342a
Sudevdeva1729@gmail.com <total@35>
2017-11-18 22:33:18 +0530

    Added Connection String in App.config

--- Faa/Action.cs
+++ Faa/Action.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace Faa
 {
@@ -17,15 +18,10 @@ namespace Faa
         /// <returns></returns>
         public SqlConnection getConnection()
         {
-            //    string constring = @"Data Source=DESKTOP-OL65NEI\SQLEXPRESS;Initial Catalog=faaDB;Integrated Security=True;Pooling=False";
-            //    string testConstring = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename="
-            //        + Path.GetDirectoryName(Application.ExecutablePath)
-            //        + "\\App_Data\\faaDB.mdf;Initial Catalog=faaDB;Integrated Security=True;Pooling=False";
-            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename="
-                + Path.GetDirectoryName(Application.ExecutablePath)
-                + "\\App_Data\\faaDB.mdf;Initial Catalog=faaDB;Integrated Security=True;Pooling=False";
-
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\App_Data\faaDB.mdf;
+                                        Initial Catalog=faaDB;Integrated Security=True;Pooling=False";
+            string newConString = ConfigurationManager.ConnectionStrings["ConnectionStringFaaDB"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(newConString);
             return sqlConnection;
         }
     }
